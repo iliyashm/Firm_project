5a2661699488954d33268db14831d7b8b3aedafd
iliyashm <58186858+iliyashm@users.noreply.github.com>
2022-04-18 12:54:48 +0300

    Add files via upload

---
+++ DataBaseFirm/Top5Month.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace DataBaseFirm
+{
+    partial class Top5Month
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Top5Month));
+            this.firmDataSet = new DataBaseFirm.FirmDataSet();
+            this.top_5_monthBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.top_5_monthTableAdapter = new DataBaseFirm.FirmDataSetTableAdapters.top_5_monthTableAdapter();
+            this.tableAdapterManager = new DataBaseFirm.FirmDataSetTableAdapters.TableAdapterManager();
+            this.top_5_monthBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.top_5_monthBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.top_5_monthDataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.firmDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.top_5_monthBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.top_5_monthBindingNavigator)).BeginInit();
+            this.top_5_monthBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.top_5_monthDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // firmDataSet
+            // 
+            this.firmDataSet.DataSetName = "FirmDataSet";
+            this.firmDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // top_5_monthBindingSource
+            // 
+            this.top_5_monthBindingSource.DataMember = "top_5_month";
+            this.top_5_monthBindingSource.DataSource = this.firmDataSet;
+            // 
+            // top_5_monthTableAdapter
+            // 
+            this.top_5_monthTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.Connection = null;
+            this.tableAdapterManager.goodsTableAdapter = null;
+            this.tableAdapterManager.salesTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = DataBaseFirm.FirmDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.usersTableAdapter = null;
+            this.tableAdapterManager.warehouse1TableAdapter = null;
+            this.tableAdapterManager.warehouse2TableAdapter = null;
+            // 
+            // top_5_monthBindingNavigator
+            // 
+            this.top_5_monthBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.top_5_monthBindingNavigator.BindingSource = this.top_5_monthBindingSource;
+            this.top_5_monthBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.top_5_monthBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.top_5_monthBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.top_5_monthBindingNavigatorSaveItem});
+            this.top_5_monthBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.top_5_monthBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.top_5_monthBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.top_5_monthBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.top_5_monthBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.top_5_monthBindingNavigator.Name = "top_5_monthBindingNavigator";
+            this.top_5_monthBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.top_5_monthBindingNavigator.Size = new System.Drawing.Size(234, 25);
+            this.top_5_monthBindingNavigator.TabIndex = 0;
+            this.top_5_monthBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 22);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // top_5_monthBindingNavigatorSaveItem
+            // 
+            this.top_5_monthBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.top_5_monthBindingNavigatorSaveItem.Enabled = false;
+            this.top_5_monthBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("top_5_monthBindingNavigatorSaveItem.Image")));
+            this.top_5_monthBindingNavigatorSaveItem.Name = "top_5_monthBindingNavigatorSaveItem";
+            this.top_5_monthBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 20);
+            this.top_5_monthBindingNavigatorSaveItem.Text = "Save Data";
+            // 
+            // top_5_monthDataGridView
+            // 
+            this.top_5_monthDataGridView.AutoGenerateColumns = false;
+            this.top_5_monthDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.top_5_monthDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1});
+            this.top_5_monthDataGridView.DataSource = this.top_5_monthBindingSource;
+            this.top_5_monthDataGridView.Location = new System.Drawing.Point(36, 28);
+            this.top_5_monthDataGridView.Name = "top_5_monthDataGridView";
+            this.top_5_monthDataGridView.Size = new System.Drawing.Size(144, 232);
+            this.top_5_monthDataGridView.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отмена";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "name";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Наименование";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // Top5Month
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 337);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.top_5_monthDataGridView);
+            this.Controls.Add(this.top_5_monthBindingNavigator);
+            this.Name = "Top5Month";
+            this.Text = "Top5Month";
+            this.Load += new System.EventHandler(this.Top5Month_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.firmDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.top_5_monthBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.top_5_monthBindingNavigator)).EndInit();
+            this.top_5_monthBindingNavigator.ResumeLayout(false);
+            this.top_5_monthBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.top_5_monthDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FirmDataSet firmDataSet;
+        private System.Windows.Forms.BindingSource top_5_monthBindingSource;
+        private FirmDataSetTableAdapters.top_5_monthTableAdapter top_5_monthTableAdapter;
+        private FirmDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator top_5_monthBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton top_5_monthBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView top_5_monthDataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+    }
+}
---
+++ DataBaseFirm/Top5Month.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBaseFirm
+{
+    public partial class Top5Month : Form
+    {
+        public Top5Month()
+        {
+            InitializeComponent();
+        }
+
+        private void Top5Month_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'firmDataSet.top_5_month' table. You can move, or remove it, as needed.
+            this.top_5_monthTableAdapter.Fill(this.firmDataSet.top_5_month);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Goods gd = new Goods();
+            gd.Show();
+            this.Hide();
+        }
+    }
+}
